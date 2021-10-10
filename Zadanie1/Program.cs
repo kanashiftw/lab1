6e2f9aff3049fde993e44bee87e57e790e3212c3
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-10-10 13:08:48 +0300

    Create Program.cs

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            Console.WriteLine("Введите число");
+            a = float.Parse(Console.ReadLine());
+            int b = (int)a;
+            float c = (a - b)*10;
+            c = (int)c;
+            Console.WriteLine(c);
+        }
+    }
+}

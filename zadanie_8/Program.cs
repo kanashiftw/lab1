6faa59efad71f8545309acf36d41745aead286eb
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-10-10 13:58:02 +0300

    Add files via upload

---
+++ zadanie_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zadanie_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, ans;
+            Console.WriteLine("Введите значение x");
+            x = double.Parse(Console.ReadLine());
+            ans = (((3 * x - 5)*x+2)*x-1)*x+7;
+            Console.WriteLine($"Ответ: {ans}");
+        }
+    }
+}

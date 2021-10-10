3143b85a4574635260f3183376b52169ad4706a6
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-10-10 13:35:40 +0300

    Add files via upload

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time,h,m,s;
+            Console.WriteLine("Введите прошедшее время (в секундах):");
+            time = int.Parse(Console.ReadLine());
+            h = time / 3600;
+            m = ((time - (h * 3600)) / 60);
+            s = (time - (h * 3600) - (m * 60));
+            Console.WriteLine($"Прошло {h} часов, {m} минут, {s} секунд");
+        }
+    }
+}

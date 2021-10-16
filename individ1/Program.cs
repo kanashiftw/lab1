b9fba20c0048241ee1dbcfe2a46e59a4ebb2a2b9
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-10-16 14:41:55 +0300

    Add files via upload

---
+++ individ1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+
+namespace individ1
+{
+    public class OOP
+    {
+        public enum Type
+        {
+            F, US
+        }
+        public class Country
+        {
+            public String StateName;
+            public String Capital;
+            public int population;
+            public Type CountryType;
+            public Country(string StateName, string Capital, int population, Type CountryType)
+            {
+                this.StateName = StateName;
+                this.Capital = Capital;
+                this.population = population;
+                this.CountryType = CountryType;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"|{this.StateName,-25}|{this.Capital,-13}|{this.population,-21}|{this.CountryType,-16}|");
+            }
+        }
+        private static void Main()
+        {
+            ArrayList list = new();      
+            bool point = true;
+            while (point)
+            {
+                Console.WriteLine("Введите данные:");
+
+                Console.WriteLine("Название гос-ва:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Столица:");
+                string Capital = Console.ReadLine();
+                Console.WriteLine("Кол-во населения:");
+                int population = int.Parse(Console.ReadLine());
+                Type CountryType;
+                while (true)
+                {
+                    Console.WriteLine("Перечисляемый тип(F, US):");
+                    string fg = Console.ReadLine();
+                    if (fg == "F")
+                    {
+                        CountryType = Type.F;
+                        break;
+                    }
+                    else if (fg == "US")
+                    {
+                        CountryType = Type.US;
+                        break;
+                    }
+                    else Console.WriteLine("Некорректный ввод значения. Введите еще раз.");
+                }             
+                Country value = new(name, Capital, population, CountryType);
+                list.Add(value);
+                while (true)
+                {
+                    Console.WriteLine("Добавить элементы в таблицу? (да - продолжить, нет - вывести таблицу)");
+                    string input = Console.ReadLine();
+                    if (input == "да" || input == "нет")
+                    {
+                        if (input == "нет")
+                        {
+                            point = false;
+                            break;
+                        }
+                        break;
+                    }
+                    else Console.WriteLine("Некорректный ответ, попробуйте еще раз");
+                }
+            }
+            Console.WriteLine(new String('-', 80));
+            Console.WriteLine($"{"|География",-79}|");
+            Console.WriteLine(new String('-', 80));
+            Console.WriteLine($"{"|Государство",-26}|{"Столица",-13}|{"Население",-21}|{"Тип",-16}|");
+            Console.WriteLine(new String('-', 79));
+            foreach (Country item in list)
+            {
+                item.Print();
+                Console.WriteLine(new String('-', 80));
+            }
+            Console.WriteLine($"{"|Перечисляемый тип: F – федерация, US - унитарное гос-во",-79}|");
+            Console.WriteLine(new String('-', 80));
+        }
+    }
+}
